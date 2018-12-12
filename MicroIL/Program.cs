bf4128f278a65b3d4828c6d93859da7edce54baa
samuel grahame <samuel@intelogy.com.au>
2018-12-12 16:58:19 +1100

    Add project files.

---
+++ MicroIL/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MicroIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+            
+
+        }
+
+        static void Run(MicroOpCode[] opCodes)
+        {
+
+        }
+
+        class MicroOpCode
+        {
+            public MicroOpCodes OpCodes;
+            public int Argument;
+        }
+
+        enum MicroOpCodes
+        {
+            LoadVar,
+            StoreVar,
+            CreateNew,
+            LoadField
+        }
+    }
+}
